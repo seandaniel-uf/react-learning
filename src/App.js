577919f1981eb874d67c0e3abc2e6f5add35c53f
seandaniel-uf <sean.degroot@uberflip.com>
2022-12-08 12:43:57 -0500

    Feat: 2-multiple-inputs complete section-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import ErrorExample from './tutorial/1-useState/setup/1-error-example';
 // import UseStateBasics from "./tutorial/1-useState/setup/3-useState-array";
 import Final from "./tutorial/1-useState/setup/3-useState-array";
-import Current from "./tutorial/4-forms/setup/1-controlled-inputs";
+import Current from "./tutorial/4-forms/setup/2-multiple-inputs";
 
 function App() {
   return (
