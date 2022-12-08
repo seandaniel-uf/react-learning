6b83b769fe01bb812872fd16c0896277c1ecdeae
seandaniel-uf <sean.degroot@uberflip.com>
2022-12-07 20:25:26 -0500

    Finished 1-controlled-inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import ErrorExample from './tutorial/1-useState/setup/1-error-example';
 // import UseStateBasics from "./tutorial/1-useState/setup/3-useState-array";
 import Final from "./tutorial/1-useState/setup/3-useState-array";
-import UseEffectBasics from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Current from "./tutorial/4-forms/setup/1-controlled-inputs";
 
 function App() {
   return (
     <div className="container">
-      <UseEffectBasics />
+      <Current />
     </div>
   );
 }
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +7,82 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
+
+  // useEffect(() => {
+  //   handleSubmit();
+  // }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      // es6 if the propery matches the variable
+      const person = { id: new Date().getTime().toString(), firstName, email };
+
+      // setPeople((people) => {
+      //   // spread.. original people plus new person
+      //   return [...people, person];
+      // });
+
+      // without spread
+      setPeople((people) => {
+        people.push(person);
+        return people;
+      });
+
+      setFirstName("");
+      setEmail("");
+
+      console.log(person);
+      // const person = {
+      //   email: email,
+      //   firstName: firstName,
+      // };
+    } else {
+      console.log("Empty Values");
+    }
+  };
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit">add person</button>
+        </form>
+        <ul>
+          {people.map(({ id, firstName, email }) => {
+            return (
+              <li key={id}>
+                <p>{firstName}</p>
+                <p>{email}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
