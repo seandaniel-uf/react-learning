08432203f6e043e6d8d65de0b1b3673f3f3d2911
seandaniel-uf <sean.degroot@uberflip.com>
2022-12-16 11:56:49 -0500

    Feat: completed useRef section-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import ErrorExample from './tutorial/1-useState/setup/1-error-example';
 // import UseStateBasics from "./tutorial/1-useState/setup/3-useState-array";
 import Final from "./tutorial/1-useState/setup/3-useState-array";
-import Current from "./tutorial/4-forms/setup/2-multiple-inputs";
+import Current from "./tutorial/5-useRef/setup/1-useRef-basics";
 
 function App() {
   return (
--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  // can be any name, returns an object {current: null}
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+  console.log(refContainer);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+  };
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div ref={divContainer}>Hello World</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
