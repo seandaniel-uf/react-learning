6898b35337b4f443605c9bc9c51d5793cc380133
seandaniel-uf <sean.degroot@uberflip.com>
2022-12-20 10:31:17 -0500

    Feat: Finished section-4 useReducer section-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import ErrorExample from './tutorial/1-useState/setup/1-error-example';
 // import UseStateBasics from "./tutorial/1-useState/setup/3-useState-array";
 import Final from "./tutorial/1-useState/setup/3-useState-array";
-import Current from "./tutorial/5-useRef/setup/1-useRef-basics";
+import Current from "./tutorial/6-useReducer/setup/index.js";
 
 function App() {
   return (
---
+++ src/tutorial/6-useReducer/setup/hooksVersion.js
@@ -0,0 +1,52 @@
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
+
+// reducer function, guard rail for updating pieces of state in the same way
+
+const Index = () => {
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name) {
+      setShowModal(true);
+      const person = {
+        id: new Date().getTime().toString,
+        name: name,
+      };
+      setPeople([...people, person]);
+    } else {
+      setShowModal(true);
+    }
+  };
+
+  return (
+    <>
+      {showModal && <Modal />}
+
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add Person</button>
+      </form>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Index;
