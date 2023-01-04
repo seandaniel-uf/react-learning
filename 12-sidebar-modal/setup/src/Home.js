c68cdb82ee1ecc0566433c473cffeb3bc3d89493
seandaniel-uf <sean.degroot@uberflip.com>
2023-01-04 09:50:43 -0500

    Feat: Completed sidebar-modal

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,21 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
 
 const Home = () => {
-  return <h2>home component</h2>
-}
+  // const data = useContext(AppContext);
+  const { openModal, openSidebar } = useGlobalContext();
 
-export default Home
+  return (
+    <main>
+      <button className="sidebar-toggle">
+        <FaBars onClick={openSidebar} />
+      </button>
+      <button onClick={openModal} className="btn">
+        Show Modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React, { useContext } from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { AppContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  const { closeSidebar, isSidebarOpen } = useContext(AppContext);
+  return (
+    <aside className={isSidebarOpen ? `sidebar show-sidebar` : `sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="Coding Addict" />
+        <button className="close-btn">
+          <FaTimes onClick={closeSidebar} />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map(({ id, url, text, icon }) => {
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map(({ id, url, icon }) => {
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
