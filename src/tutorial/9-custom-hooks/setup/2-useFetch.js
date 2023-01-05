01178fd08952ad84e3c9ef94431c76d4167f141b
seandaniel-uf <sean.degroot@uberflip.com>
2023-01-04 20:19:12 -0500

    Feat: Completed custom hooks section-4

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    // response, and then JSON
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+
+    // axios
+    // const { data } = await axios.get(url);
+
+    // console.log(data);
+    // setProducts(data);
+    // setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+
+  return { loading, products };
+};
